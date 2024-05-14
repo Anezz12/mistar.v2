1a8b6247ac1d5ec60e39ca69fce8be3014f070f3
Anezz12 <harsenaargrtya1@gmail.com>
2024-05-14 07:54:41 +0700

    update About & Service

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 function page() {
   return (
+    // Navbar navigation
     <div className="relative py-20">
       <div className="absolute inset-0 z-0">
         <div
@@ -28,6 +30,190 @@ function page() {
           <div className="flex items-center gap-4 sm:flex-col"></div>
         </header>
       </div>
+      {/* About */}
+      <div>
+        {/* <div className="py-20 flex flex-col items-center">
+          <Image
+            src="/bg2.jpg"
+            width="1000"
+            height="1000"
+            alt="some value"
+            className="items-center rounded-md"
+          />
+        </div> */}
+        <section className="w-[90vw] mx-auto my-32 flex flex-col items-center gap-8">
+          <div
+          // className="w-full rounded-md bg-cover bg-center h-[120vh]"
+          // style={{
+          //   backgroundSize: "cover",
+          //   backgroundPosition: "center",
+          //   backgroundImage: `url("/bg2.jpg")`,
+          // }}
+          >
+            <Image
+              src="/bg2.jpg"
+              alt="Image"
+              width={1000}
+              height={1000}
+              className="w-full h-fit rounded-md"
+            />
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
+            <div className="item w-full flex flex-col gap-4">
+              <div className="title text-2xl font-bold text-black">
+                Siapa kami?
+              </div>
+              <div className="text text-gray-500 text-justify">
+                Kami lebih dari sekadar layanan fotokopi. Kami adalah tim
+                profesional berdedikasi yang bersemangat memberikan layanan yang
+                luar biasa. Perjalanan kami dimulai dengan visi untuk memberikan
+                pengalaman fotokopi yang memadukan kenyamanan, kualitas, dan
+                keberlanjutan ekonomi. Dengan keahlian bertahun-tahun di
+                industri fotokopi, kami telah berkembang menjadi nama yang
+                terpercaya. Kami bangga atas komitmen kami terhadap kepuasan
+                pelanggan dan praktik sadar lingkungan. Di Mistar Fotocopy, kami
+                menawarkan berbagai layanan fotokopi, mulai dari fotokopi
+                berwarna dan hitam putih, hingga pencetakan dokumen dalam
+                berbagai ukuran. Kami menggunakan peralatan modern dan teknologi
+                terbaru untuk memastikan hasil yang tajam dan jelas setiap kali
+                Anda memerlukannya. Selain itu, kami juga menyediakan layanan
+                tambahan seperti penjilidan, laminasi, dan pembuatan brosur,
+                sehingga Anda dapat memenuhi semua kebutuhan dokumen Anda di
+                satu tempat. Kami memahami pentingnya efisiensi dan kecepatan
+                dalam setiap pekerjaan, itulah sebabnya kami selalu berusaha
+                memberikan layanan cepat tanpa mengorbankan kualitas. Selain
+                itu, kami juga menerapkan praktik ramah lingkungan dengan
+                menggunakan kertas daur ulang dan bahan-bahan yang tidak merusak
+                lingkungan. Kenali wajah-wajah di balik Mistar Fotocopy dan
+                temukan layanan fotokopi yang peduli dengan kebutuhan Anda. Kami
+                siap membantu Anda dengan layanan yang ramah, profesional, dan
+                dapat diandalkan. Mistar Fotocopy adalah pilihan tepat untuk
+                segala kebutuhan fotokopi Anda.
+              </div>
+            </div>
+            <div className="item w-full flex flex-col gap-4">
+              <div className="title text-2xl font-bold text-black">
+                Apa yang kami kerjakan?
+              </div>
+              <div className="text text-gray-500 text-justify">
+                Misi inti kami sederhana: membuat hidup Anda lebih mudah dan
+                kebutuhan dokumen Anda terpenuhi dengan cepat dan efisien. Kami
+                menawarkan layanan komprehensif yang mencakup fotokopi,
+                pencetakan, penjilidan, laminasi, dan pembuatan brosur. Setiap
+                layanan dirancang dengan perhatian terhadap detail dan kualitas,
+                memastikan hasil yang memuaskan setiap kali. Opsi penjemputan
+                dan pengantaran kami yang efisien dirancang agar sesuai dengan
+                jadwal Anda, memberikan Anda kenyamanan yang layak Anda
+                dapatkan. Anda tidak perlu khawatir tentang waktu atau jarak;
+                kami siap melayani Anda dengan fleksibilitas maksimal,
+                memastikan bahwa semua kebutuhan dokumen Anda terpenuhi tepat
+                waktu. Kami memahami bahwa setiap dokumen memiliki nilai dan
+                pentingnya masing-masing. Oleh karena itu, selain kecepatan,
+                kami memprioritaskan kualitas hasil cetakan dengan menggunakan
+                peralatan modern dan teknologi terbaru. Hasil cetakan kami
+                selalu tajam, jelas, dan profesional, sesuai dengan standar
+                tertinggi dalam industri fotokopi. Selain efisiensi dan
+                kualitas, kami juga memprioritaskan keberlanjutan dan
+                kesejahteraan bumi kita. Melalui praktik ramah lingkungan
+                seperti menggunakan kertas daur ulang dan bahan-bahan yang tidak
+                merusak lingkungan, kami berkomitmen untuk menjaga kelestarian
+                lingkungan sambil menyediakan layanan terbaik. Kami percaya
+                bahwa setiap langkah kecil menuju keberlanjutan memiliki dampak
+                besar bagi masa depan kita bersama.
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      <section className="w-[80vw] mx-auto my-32 flex gap-8 mobile:flex-col-reverse">
+        <div className=" w-2/5 left-0 flex flex-col items-end gap-8">
+          <div className="font-bold text-3xl text-right">
+            Bersantai dalam Kenyamanan Murni
+          </div>
+          <div className="items flex flex-col items-end gap-4">
+            <div className="item flex items-center gap-4 text-center mobile:flex-col-reverse">
+              <div className="flex flex-col items-end">
+                <div className="font-bold text-3xl text-black/[.7]">7</div>
+                <p className=" text-sm text-black/[.7]">Mesin Fotocopy</p>
+              </div>
+              <div className="flex justify-center items-center p-4 rounded-full">
+                <Image
+                  src="/mesin1.png"
+                  width="40"
+                  height="100"
+                  alt="some value"
+                  className="items-center"
+                />
+              </div>
+            </div>
+            <div className=" flex items-center gap-4 text-center">
+              <div className=" flex flex-col items-end">
+                <div className="font-bold text-3xl text-black/[.7]">2</div>
+                <p className=" text-sm text-black/[.7]">Cabang</p>
+              </div>
+              <div className="flex justify-center items-center p-4 rounded-full">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="40"
+                  height="40"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="text-yellow-400"
+                >
+                  <path d="M3 21v-13l9 -4l9 4v13"></path>
+                  <path d="M13 13h4v8h-10v-6h6"></path>
+                  <path d="M13 21v-9a1 1 0 0 0 -1 -1h-2a1 1 0 0 0 -1 1v3"></path>
+                </svg>
+              </div>
+            </div>
+            <div className="flex items-center gap-4 text-center">
+              <div className="flex flex-col items-end ">
+                <div className="font-bold text-3xl text-black/[.7]">5</div>
+                <p className="text-sm text-black/[.7]">Karyawan</p>
+              </div>
+              <div className="flex justify-center items-center p-4 rounded-full">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="40"
+                  height="40"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="text-yellow-400"
+                >
+                  <path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
+                  <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+                  <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+                  <path d="M21 21v-2a4 4 0 0 0 -3 -3.85"></path>
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          className="w-full rounded-md bg-cover bg-center h-[55vh]"
+          style={{
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundImage: `url("/bg2.jpg")`,
+          }}
+        >
+          {/* <Image
+            src="/bg2.jpg"
+            alt="Image"
+            width={1000}
+            height={667}
+            className="w-full h-fit rounded-md"
+          /> */}
+        </div>
+      </section>
     </div>
   );
 }
--- app/service/page.tsx
+++ app/service/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 function page() {
   return (
@@ -28,6 +29,197 @@ function page() {
           <div className="flex items-center gap-4 sm:flex-col"></div>
         </header>
       </div>
+      <section>
+        <div>
+          <div className=" flex flex-col items-center max-w-7xl mx-auto p-5  pt-48">
+            <span className="text-black font-bold text-4xl py-10">
+              Kenapa <em className="text-blue-500">Mistar</em>
+            </span>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+              <div className="bg-slate-50 flex flex-col items-center rounded-lg shadow-md overflow-hidden">
+                <Image
+                  src="/bg.jpeg"
+                  width="200"
+                  height="300"
+                  alt="some value"
+                  className=" w-full h-48 object-cover"
+                />
+                <div className="p-10  text-center bg-slate-50">
+                  <h3 className="text-lg font-semibold mb-2 text-black">
+                    Fotocopy Terlengkap
+                  </h3>
+                  <p className="text-gray-600 mb-4">
+                    Kami menyediakan alat tulis kantor / ATK, PC untuk ngetik
+                    dan print, jasa cutting, jilid skripsi kilat, dll.
+                  </p>
+                </div>
+              </div>
+
+              <div className="bg-slate-50 flex flex-col items-center rounded-lg shadow-md overflow-hidden">
+                <Image
+                  src="/bg.jpeg"
+                  width="300"
+                  height="300"
+                  alt="some value"
+                  className=" w-full h-48 object-cover"
+                />
+                <div className="p-10  text-center bg-slate-50">
+                  <h3 className="text-lg font-semibold mb-2 text-black">
+                    Peralatan Terbaik
+                  </h3>
+                  <p className="text-gray-600 mb-4">
+                    Kami menggunakan mesin fotocopy terbaru dan terbaik saat ini
+                    dan peralatan - peralatan pendukung kami yang terawat dan
+                    berkualitas, tidak usah ragu lagi dengan hasil dari pada
+                    kami.
+                  </p>
+                </div>
+              </div>
+
+              <div className="bg-slate-50 flex flex-col items-center rounded-lg shadow-md overflow-hidden">
+                <Image
+                  src="/bg.jpeg"
+                  width="300"
+                  height="300"
+                  alt="some value"
+                  className=" w-full h-48 object-cover"
+                />
+                <div className="p-10  text-center bg-slate-50">
+                  <h3 className="text-lg font-semibold mb-2 text-black">
+                    Layanan Terbaik
+                  </h3>
+                  <p className="text-gray-600 mb-4">
+                    Dengan tenaga profesional dan berketerampilan khusus di
+                    bidangnya, anda tidak perlu khawatir lagi dengan hasil kerja
+                    kami, karna kami akan memberikan pelayanan yang prima kepada
+                    seluruh pelanggan kami.
+                  </p>
+                </div>
+              </div>
+              <div className="bg-slate-50 flex flex-col items-center rounded-lg shadow-md overflow-hidden">
+                <Image
+                  src="/bg.jpeg"
+                  width="300"
+                  height="300"
+                  alt="some value"
+                  className=" w-full h-48 object-cover"
+                />
+                <div className="p-10  text-center bg-slate-50">
+                  <h3 className="text-lg font-semibold mb-2 text-black">
+                    Layanan Terbaik
+                  </h3>
+                  <p className="text-gray-600 mb-4">
+                    Dengan tenaga profesional dan berketerampilan khusus di
+                    bidangnya, anda tidak perlu khawatir lagi dengan hasil kerja
+                    kami, karna kami akan memberikan pelayanan yang prima kepada
+                    seluruh pelanggan kami.
+                  </p>
+                </div>
+              </div>
+              <div className="bg-slate-50 flex flex-col items-center rounded-lg shadow-md overflow-hidden">
+                <Image
+                  src="/bg.jpeg"
+                  width="300"
+                  height="300"
+                  alt="some value"
+                  className=" w-full h-48 object-cover"
+                />
+                <div className="p-10  text-center bg-slate-50">
+                  <h3 className="text-lg font-semibold mb-2 text-black">
+                    Layanan Terbaik
+                  </h3>
+                  <p className="text-gray-600 mb-4">
+                    Dengan tenaga profesional dan berketerampilan khusus di
+                    bidangnya, anda tidak perlu khawatir lagi dengan hasil kerja
+                    kami, karna kami akan memberikan pelayanan yang prima kepada
+                    seluruh pelanggan kami.
+                  </p>
+                </div>
+              </div>
+              <div className="bg-slate-50 flex flex-col items-center rounded-lg shadow-md overflow-hidden">
+                <Image
+                  src="/bg.jpeg"
+                  width="300"
+                  height="300"
+                  alt="some value"
+                  className=" w-full h-48 object-cover"
+                />
+                <div className="p-10  text-center bg-slate-50">
+                  <h3 className="text-lg font-semibold mb-2 text-black">
+                    Layanan Terbaik
+                  </h3>
+                  <p className="text-gray-600 mb-4">
+                    Dengan tenaga profesional dan berketerampilan khusus di
+                    bidangnya, anda tidak perlu khawatir lagi dengan hasil kerja
+                    kami, karna kami akan memberikan pelayanan yang prima kepada
+                    seluruh pelanggan kami.
+                  </p>
+                </div>
+              </div>
+              <div className="bg-slate-50 flex flex-col items-center rounded-lg shadow-md overflow-hidden">
+                <Image
+                  src="/bg.jpeg"
+                  width="300"
+                  height="300"
+                  alt="some value"
+                  className=" w-full h-48 object-cover"
+                />
+                <div className="p-10  text-center bg-slate-50">
+                  <h3 className="text-lg font-semibold mb-2 text-black">
+                    Layanan Terbaik
+                  </h3>
+                  <p className="text-gray-600 mb-4">
+                    Dengan tenaga profesional dan berketerampilan khusus di
+                    bidangnya, anda tidak perlu khawatir lagi dengan hasil kerja
+                    kami, karna kami akan memberikan pelayanan yang prima kepada
+                    seluruh pelanggan kami.
+                  </p>
+                </div>
+              </div>
+              <div className="bg-slate-50 flex flex-col items-center rounded-lg shadow-md overflow-hidden">
+                <Image
+                  src="/bg.jpeg"
+                  width="300"
+                  height="300"
+                  alt="some value"
+                  className=" w-full h-48 object-cover"
+                />
+                <div className="p-10  text-center bg-slate-50">
+                  <h3 className="text-lg font-semibold mb-2 text-black">
+                    Layanan Terbaik
+                  </h3>
+                  <p className="text-gray-600 mb-4">
+                    Dengan tenaga profesional dan berketerampilan khusus di
+                    bidangnya, anda tidak perlu khawatir lagi dengan hasil kerja
+                    kami, karna kami akan memberikan pelayanan yang prima kepada
+                    seluruh pelanggan kami.
+                  </p>
+                </div>
+              </div>
+              <div className="bg-slate-50 flex flex-col items-center rounded-lg shadow-md overflow-hidden">
+                <Image
+                  src="/bg.jpeg"
+                  width="300"
+                  height="300"
+                  alt="some value"
+                  className=" w-full h-48 object-cover"
+                />
+                <div className="p-10  text-center bg-slate-50">
+                  <h3 className="text-lg font-semibold mb-2 text-black">
+                    Layanan Terbaik
+                  </h3>
+                  <p className="text-gray-600 mb-4">
+                    Dengan tenaga profesional dan berketerampilan khusus di
+                    bidangnya, anda tidak perlu khawatir lagi dengan hasil kerja
+                    kami, karna kami akan memberikan pelayanan yang prima kepada
+                    seluruh pelanggan kami.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
