b0e5a21edd32b5ff2404a1c2dc3d3ca1e97578d2
Anezz12 <harsenaargrtya1@gmail.com>
2024-05-20 02:32:35 +0700

    update theme

--- components/theme/theme-switch.tsx
+++ components/theme/theme-switch.tsx
@@ -1,15 +1,34 @@
 "use client";
 
 import { useTheme } from "@/ui/theme-context";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BsMoon, BsSun } from "react-icons/bs";
 
 export default function ThemeSwitch() {
   const { theme, toggleTheme } = useTheme();
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollHeight = window.pageYOffset;
+      if (scrollHeight > 200) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <button
-      className="fixed bottom-5 right-5 bg-white text-black w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950 dark:text-white"
+      className={`fixed bottom-5 right-5 bg-white transition-opacity duration-300 text-black w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 dark:bg-gray-950 dark:text-white ${
+        showButton ? "opacity-100" : "opacity-0"
+      }`}
       onClick={toggleTheme}
     >
       {theme === "light" ? <BsSun /> : <BsMoon />}
